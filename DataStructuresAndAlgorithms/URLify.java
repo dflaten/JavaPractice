7fcae3cf2735f95ff32bd627ee9b8c52b5047178
discocorg <david.flaten@gmail.com>
2023-04-03 17:29:59 -0500

    Adding URLify problem outline.

---
+++ DataStructuresAndAlgorithms/URLify.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+/*
+ * Problem: URLify: Replace all spaces in a string with `%20`. Assume the string has space at the end to hold the additional characters
+ *                  and the length of the string is also provided. 
+ *
+ *                  Example: Input: "Mr John Smith      ", 13
+ *                           Output: "Mr%20John@20Smith"
+ *
+ */
+public class URLify {
+
+    // My Implementation: 1) 
+    //                    2) 
+    //                    3) 
+    //                    4) 
+    //
+    // Assumptions: 
+    // 
+    // Mistakes: 
+    // Big O: 
+    static public String urlify(String string, int finalLength) {
+	//Edge Cases
+	return string;
+
+    }
+
+
+    public static void main(String args[]) {
+	System.out.println("Starting Program!");
+	System.out.println("--------");
+	System.out.println("Using the urlify solution this string, /Mr John Smith/, produced : " + urlify("Mr John Smith", 13));
+
+
+    }
+}
