48e0527a8b2cb36ab4abc1419adc42da979b1b12
discocorg <david.flaten@gmail.com>
2023-04-03 17:20:37 -0500

    Added solution for actual StringPermutation problem after confirming assumptions.

--- DataStructuresAndAlgorithms/StringPermutationDetector.java
+++ DataStructuresAndAlgorithms/StringPermutationDetector.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Arrays;
 
 /*
  * Problem: Check Permutations: Given two Strings, write a method to decide if one is a permutation of the
@@ -61,6 +62,41 @@ public class StringPermutationDetector {
 	return foundSubStringPermutation;
 
     }
+    // Solution after confirming the assumptions by looking at the answer in the book. (I misunderstood permutation in solution 1). h
+    // My Implementation: 1) Make Sure strings are both the same length 
+    //                    2) Sort Each String. 
+    //                    3) See if they are Equal. 
+    //
+    // Assumptions: (Would ask as a question in interview). If either string is null will return false. If both strings empty return false. 
+    // 
+    // Mistakes: At first I thought two HashMaps would provide a good solution but realized it wouldn't because each string could have
+    //           repeat characters.
+    //           Didn't add a sort method outside of the function to help re-use code for sorting.
+    // Big O: O(n + j + n log(n) + j log(j) + n * j) or O(nj) simplified.
+    static public boolean stringPermutationDetectorCorrectAssumptions(String s1, String s2) {
+	//Edge Cases
+	if (s1 == null || s2 == null) {
+	    return false;
+	}
+	if (s1 == "" || s2 == "") {
+	    return false;
+	}
+	if (s1.length() != s2.length()){
+	    return false;
+	}
+	char s1Array [] = s1.toCharArray();
+	char s2Array [] = s2.toCharArray();
+
+	Arrays.sort(s1Array);
+	Arrays.sort(s2Array);
+	
+	String sortedS1 = String.valueOf(s1Array);
+	String sortedS2 = String.valueOf(s2Array);
+
+	return sortedS1.equals(sortedS2);
+	
+    }
+
 
     public static void main(String args[]) {
 	System.out.println("Starting Program!");
@@ -68,6 +104,9 @@ public class StringPermutationDetector {
 	System.out.println("Using the stringPermutationDetector solution this string, string, is a permutation of this string, corgistring, : " + stringPermutationDetector("string", "corgistring"));
 	System.out.println("Using the stringPermutationDetector solution this string, dog, is a permutation of this string, corgidoggood, : " + stringPermutationDetector("corgidoggood", "dog"));
 	System.out.println("Using the stringPermutationDetector solution this string, cat, is not a permutation of this string, corgidoggood, : " + stringPermutationDetector("corgidoggood", "cat"));
+	System.out.println("Using the stringPermutationDetectorCorrectAssumptions solution this string, cat, is not a permutation of this string, bat, : " + stringPermutationDetectorCorrectAssumptions("bat", "cat"));
+	System.out.println("Using the stringPermutationDetectorCorrectAssumptions solution this string, cat, is a permutation of this string, tac, : " + stringPermutationDetectorCorrectAssumptions("tac", "cat"));
+	System.out.println("Using the stringPermutationDetectorCorrectAssumptions solution this string, catcat, is a permutation of this string, tactac, : " + stringPermutationDetectorCorrectAssumptions("tactac", "catcat"));
 
 
     }
