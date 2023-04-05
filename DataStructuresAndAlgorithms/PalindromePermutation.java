2f54d72e32dedeab961ec4b34f6f1610efdbd111
discocorg <david.flaten@gmail.com>
2023-04-04 21:58:08 -0500

    Adding solution for PalindromePermutation problem.

--- DataStructuresAndAlgorithms/PalindromePermutation.java
+++ DataStructuresAndAlgorithms/PalindromePermutation.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Arrays;
 
 /*
@@ -22,18 +23,53 @@ public class PalindromePermutation {
     //                    3) If it does return true, if not return false;
     //
     // Assumptions: I am understanding that given a string we want to know if we can re-arrange the characters in it to
-    //              create a palindrome. Also assuming we just want a true/false back not the palindrome that we found.
+    //              create a palindrome. 
+    //              Also assuming we just want a true/false back not the palindrome that we found.
+    //              Assuming the letters are all English.
     // 
-    // Mistakes:  
-    // Big O: 
+    // Mistakes: I used the `toLowerCase(), and replaceAll() String methods here which may not be as efficient as adding
+    //           a check during the counting of characters to only count values within the expected range (97 - 122) 
+    //           assuming ASCII. I put O(n) here but due to using String methods it is possible it is greater.
+    // Big O: O(n)
+    static boolean isOdd(int i) {
+	if(i % 2 == 0) {
+	    return false;
+	}
+	return true;
+    }
     static public boolean permutationPalindrome(String string) {
-	
 	//Edge Cases
+	String ONLY_LOWER_CASE_LETTERS = "[^a-z]";
 	if (string == "") {
 	    return false;
 	}
 
-	return true;
+	String cleanedString = string.toLowerCase(Locale.ENGLISH).replaceAll(ONLY_LOWER_CASE_LETTERS, "");
+
+	int [] letterCounter = new int[173];
+
+	int oddNumberOfChars = 0;
+	for (int i = 0; i < cleanedString.length(); i++) {
+	    letterCounter[cleanedString.charAt(i)] = letterCounter[cleanedString.charAt(i)] + 1;
+	}
+
+   	for (int j = 97; j < 123; j++) {
+	    if (isOdd(letterCounter[j])) {
+		oddNumberOfChars++;
+	    }
+	}
+
+	if(isOdd(cleanedString.length())) {
+	    if (oddNumberOfChars == 1) {
+		return true;
+	    }
+	    return false;
+	}
+
+	if (oddNumberOfChars == 0) {
+	    return true;
+	}
+	return false;
     }
 
 
@@ -41,7 +77,8 @@ public class PalindromePermutation {
 	System.out.println("Starting Program!");
 	System.out.println("--------");
 	System.out.println("Using the permutationPalindrome solution this string, //, produced : " + permutationPalindrome(""));
-
-
+	System.out.println("Using the permutationPalindrome solution this string, /Tact Coa/, produced : " + permutationPalindrome("Tact Coa"));
+	System.out.println("Using the permutationPalindrome solution this string, /Ract Coa/, produced : " + permutationPalindrome("Ract Coa"));
+	System.out.println("Using the permutationPalindrome solution this string, /aa bb/, produced : " + permutationPalindrome("aa bb"));
     }
 }
