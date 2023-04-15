6445e7f286767088c5caa32ca486789d8c8857df
discocorg <david.flaten@gmail.com>
2023-04-15 14:52:44 -0500

    Completed StackOfPlates problem.

--- DataStructuresAndAlgorithms/StacksAndQueues/StackOfPlates.java
+++ DataStructuresAndAlgorithms/StacksAndQueues/StackOfPlates.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
  * This Set of Stacks will work in the same way. It will store data in a Stack
  * until it reaches a certain limit, then it will add another Stack in the set
  * to store data in. The Set of Stacks will continue to grow in this way. 
+ * 
+ * Followup: How would you change your implementation if you wanted a method
+ * that allowed you to choose which substack you wanted to pop an item off at?
+ * so popAt(int stackNumber);
  *
  * However the pop/push methods should still work the exact same way as a
  * normal Stack.
@@ -19,7 +23,7 @@ import java.util.ArrayList;
  *              stack could be entered twice if the same elements were added
  *              to it in the same order twice in a row.
  *
- * Implementation: 
+ * Implementation: See data structure below.
  *
  * Mistakes: 
  */
@@ -103,7 +107,7 @@ class Stack {
 class SetOfStacks {
     ArrayList<Stack> stacks;
 
-    public UpgradedStack(){
+    public SetOfStacks() {
         this.stacks = new ArrayList();
     }
 
@@ -111,17 +115,19 @@ class SetOfStacks {
     // We start out, there are no stacks, we create a new stack and add it to
     // the list.
         if (this.stacks.isEmpty()) {
+            System.out.println("Creating first Stack for set!");
             stacks.add(new Stack());
         }
         // Check if any existing stacks have room. Start at end of list
         // because that is where we add new stacks.
-        for (stackNumber = stacks.size() - 1; stackNumber >= 0; stackNumber--) {
+        for (int stackNumber = stacks.size() - 1; stackNumber >= 0; stackNumber--) {
             if(!stacks.get(stackNumber).isFull()) {
                 stacks.get(stackNumber).push(value);
                 return;
             }         
         }
         //Handle case where all stacks are full
+        System.out.println("Creating new stack for set!");
         stacks.add(new Stack());
         stacks.get(stacks.size()-1).push(value);
 
@@ -134,13 +140,23 @@ class SetOfStacks {
             return Integer.MAX_VALUE;
         }
         // Pop an item off the stack last added that still has an item.
-        for (stackNumber = stacks.size() - 1; stackNumber >= 0; stackNumber--) {
+        for (int stackNumber = stacks.size() - 1; stackNumber >= 0; stackNumber--) {
             if(!stacks.get(stackNumber).isEmpty()) {
-                return stacks.get(stackNumber).pop;
-            }         
+                return stacks.get(stackNumber).pop();
+                }         
         }
         System.out.println("All Stacks in list Empty, would normally throw exception.");
         return Integer.MAX_VALUE;
+    }
+    // Implementing a function to allow which subStack you want to pop off of
+    public int popAt(int stackNumber) {
+        if(this.stacks.get(stackNumber) == null 
+                || this.stacks.get(stackNumber).isEmpty()) {
+            System.out.println("Stack Empty, would normally throw exception.");
+            return Integer.MAX_VALUE;
+        }
+
+        return this.stacks.get(stackNumber).pop();
     }
 
     public int peak() {
@@ -154,32 +170,51 @@ class SetOfStacks {
 
 }
 
-public class StackMin {
+public class StackOfPlates {
 
-    // My Implementation: 1) Create a Simple stack to track Mins
-    //                    2) Create Expanded Stack which implements a min()
-    //                       method to return the current min. Will use a 
-    //                       stack of mins to track the current min.
+    // My Implementation: 1) Create new SetOfStacks data structure which
+    //                       contains  a List of Stacks, adding isFull() 
+    //                       method to my standard Stack Implementation and
+    //                       hard coded limit of 3 items per stack.
+    //                    2) Add other standard Stack functions to my
+    //                       SetOfStacks structure.
     //
-    // Assumptions: 
+    // Assumptions: For the followup I assumed we didn't want to try to keep
+    //              each stack "full" but were more ok with having a structure
+    //              that took up a bit more space for the increased speed.
     //
-    // Mistakes: When implementing a simple stack I didn't have the else 
-    //           statement. causing the if block to execute and then the
-    //           others creating an infinite loop.
-    //
-    //           Didn't actually need a min number to track here I could have
-    //           used the Stack and just peaked at the top number at any time.
+    // Mistakes:  
     //
     // Big O: O(1) for all operations, O(2n) for space due to min stack 
 
 
-    public static UpgradedStack createUpgradedStack() {
-        UpgradedStack myStack = new UpgradedStack();
+    public static SetOfStacks createSetOfStacks() {
+        SetOfStacks myStack = new SetOfStacks();
         myStack.push(8);
         myStack.push(3);
         myStack.push(1);
-        myStack.push(3);
+        myStack.push(2);
+        myStack.push(7);
+        myStack.push(4);
         myStack.push(8);
+        return myStack;
+    }
+    public static SetOfStacks addRemoveStacksSetOfStacks() {
+        SetOfStacks myStack = new SetOfStacks();
+        myStack.push(8);
+        myStack.push(3);
+        myStack.push(1);
+        System.out.println("Popping Sub stack off!");
+        myStack.pop();
+        myStack.pop();
+        myStack.pop();
+        myStack.push(2);
+        myStack.push(7);
+        myStack.push(4);
+        myStack.pop();
+        myStack.push(8);
+        myStack.push(7);
+        myStack.push(99);
         return myStack;
     }
 
@@ -202,11 +237,11 @@ public class StackMin {
         }
     }
 
-    public static void printUpgradedStack(UpgradedStack stack) {
+    public static void printSetOfStacks(SetOfStacks stack) {
         int itemNumber = 0;
         while(!stack.isEmpty()) {
-            System.out.println("Item: " + itemNumber + " " + 
-                "Current Min: "+ stack.min()  +" Value popped: " + stack.pop());
+            System.out.println("Item: " + itemNumber +
+                " Value popped: " + stack.pop());
             itemNumber++;
         }
     }
@@ -222,8 +257,16 @@ public class StackMin {
         System.out.println("Peaking at the Simple Stack first item: ");
         System.out.println(stack2.peak());
         System.out.println("--------");
-        UpgradedStack upgradedStack = createUpgradedStack();
-        System.out.println("The Upgraded Stack created was : ");
-        printUpgradedStack(upgradedStack);
+        System.out.println("Creating Set of Stacks!");
+        SetOfStacks setOfStacks = createSetOfStacks();
+        System.out.println("Peaking at first item on Stack!");
+        System.out.println(setOfStacks.peak());
+        System.out.println("The Set Of Stacks created was : ");
+        printSetOfStacks(setOfStacks);
+        System.out.println("--------");
+        System.out.println("Testing pop/push with SetOfStacks: ");
+        SetOfStacks setOfStacks2 = addRemoveStacksSetOfStacks();
+        printSetOfStacks(setOfStacks2);
+
     }
 }
