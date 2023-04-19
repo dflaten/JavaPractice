a40e13fe86d0f5c031a348799613b1ee353f61eb
discocorg <david.flaten@gmail.com>
2023-04-19 18:45:05 -0500

    Started on Minimal Tree problem, corrected approach mid development.

--- DataStructuresAndAlgorithms/TreesAndGraphs/MinimalTree.java
+++ DataStructuresAndAlgorithms/TreesAndGraphs/MinimalTree.java
@@ -63,24 +63,57 @@ public class MinimalTree {
 
     /* My Implementation: 1) Find the mid point and use that as the root for my
      *                       tree.
-     *                    2) Iterate through the Array taking the next lowest
-     *                       number in the array and placing it on the left of
-     *                       the root and the next highest number on the right
-     *                       of the root.
-     *                    3) Then go the right and left nodes for the root and
-     *                       do the same, continuing until we get to the end
-     *                       of the array.
+     *                    2) We start building the tree first from the left.
+     *                       We take not the next lowest but the one after that
+     *                       and make it the left node of the root. Its right 
+     *                       node will be the number between the root and the
+     *                       root.left number. Repeat this pattern until we 
+     *                       get to the start of the array. 
+     *                       
+     *                    3) Do the same thing for the right side (except items
+     *                       are  greater. until we get to the end of the
+     *                       Array. Done!
      *                       
      * 
      * Assumptions: 
      * 
-     * Mistakes: 
+     * Mistakes: At first I started by finding the midpoint and iterrating
+     *          through the array putting the lowest highest as children on 
+     *          the left and then right. This obviously wouldn't result in 
+     *          a balanced Binary Tree though because there would be elements
+     *          to the left of the root greater than the right.
+     *
+     *          In the future I need to create a picture of my end state and 
+     *          work through my algorithm there before starting on
+     *          implementation.
      *           
      *           
      * Big O: 
      *         
      */
     static Node buildShortestBinaryTree(int [] arrayOfIntegers) {
+        int middle = arrayOfIntegers.length / 2;
+        Node root = new Node(middle);
+        Node current = root;
+        // Offset needed because event numbers will have one less node 
+        // on the right than the left. 
+        boolean evenArrayLength = false; 
+        if (arrayOfintegers.length % 2 == 0 ) {
+            evenArrayLength = true;
+        } else {
+            evenArrayLength = false;
+        }
+        // Iterate through the array starting in the middle
+        // How do I make sure I get all of the things in the array without
+        // trying to get something that is not in the size of the array? 
+        boolean isLeft = true;
+        for (int i = 1; i <= middle; i++) {
+               current.left = new Node(arrayOfIntegers[middle - i]); 
+               current.right = new Node([middle + i]); 
+
+               current = current.left;
+        }
+
         return new Node(0);
     }
 
@@ -89,7 +122,7 @@ public class MinimalTree {
         System.out.println("Starting Program!");
         System.out.println("--------");
         System.out.println("The shortest Binary Tree is: ");
-        System.out.println("TO BE WRITTEN");
+        buildShortestBinaryTree(createSortedArray());
         System.out.println("--------");
     }
 }
